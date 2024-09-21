e7bdbd53d585f6d90d1e057690c52dcaf97573ed
Anatoly Rozhko <anatolyrozk@gmail.com>
2024-09-21 12:36:39 +0300

    Declare cli commands

--- main.go
+++ main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
+type cliCommand struct {
+	action func() error
+	name   string
+}
+
 func main() {
+	commands := map[string]cliCommand{
+		"help": {
+			name:   "name",
+			action: helpCommand,
+		},
+		"exit": {
+			name:   "exit",
+			action: exitCommand,
+		},
+	}
+
 	fmt.Println("Hello world")
+}
+
+func helpCommand() error {
+	return nil
+}
+
+func exitCommand() error {
+	os.Exit(0)
+	return nil
 }
