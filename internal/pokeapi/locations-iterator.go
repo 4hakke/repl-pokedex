d362b0039119c72868c7c66e60f3efed4bbfe40d
Anatoly Rozhko <anatolyrozk@gmail.com>
2024-09-22 12:25:24 +0300

    Iterate locations backwards

---
+++ internal/pokeapi/locations-iterator.go
@@ -0,0 +1,33 @@
+package pokeapi
+
+type LocationsIterator struct {
+	Limit  int
+	offset int
+	count  *int
+}
+
+func (li *LocationsIterator) Next() ([]Location, error) {
+	if li.count != nil && li.offset >= *li.count {
+		return []Location{}, nil
+	}
+	res, err := locations(li.offset, li.Limit)
+	if err != nil {
+		return []Location{}, err
+	}
+	li.offset += li.Limit
+	li.count = &res.Count
+	return res.Results, nil
+}
+
+func (li *LocationsIterator) Previous() ([]Location, error) {
+	if li.offset-li.Limit < 0 {
+		return []Location{}, nil
+	}
+	li.offset -= li.Limit
+	res, err := locations(li.offset, li.Limit)
+	if err != nil {
+		return []Location{}, err
+	}
+	li.count = &res.Count
+	return res.Results, nil
+}
