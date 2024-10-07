5034fc7557d519fc7a9b4fe7640f0a72efa25755
Anatoly Rozhko <anatolyrozk@gmail.com>
2024-10-07 16:20:23 +0200

    Update help command

--- internal/repl/help-command.go
+++ internal/repl/help-command.go
@@ -12,6 +12,8 @@ func (repl *Repl) helpCommand(params []string) error {
 	fmt.Printf("%s: %s\n", commands["map"].name, commands["map"].description)
 	fmt.Printf("%s: %s\n", commands["mapb"].name, commands["mapb"].description)
 	fmt.Printf("%s: %s\n", commands["explore"].name, commands["explore"].description)
+	fmt.Printf("%s: %s\n", commands["inspect"].name, commands["inspect"].description)
+	fmt.Printf("%s: %s\n", commands["pokedex"].name, commands["pokedex"].description)
 	fmt.Println("")
 	return nil
 }
